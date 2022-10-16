a1b512cec2dafaa61a8ae3e4dfc54e69c551d7c8
Josh Streeter <Joshstreeter44@gmail.com>
2022-10-16 01:26:34 -0700

    added in code to script.js for password gen

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
+///////////////////////// DO NOT CHANGE ABOVE HERE ///////////////////////// 
 
 function generatePassword() {
-  /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
+
+  var length = Number(prompt("Enter a password length between 8 and 128")),
+  charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.")
+
+  var charSets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  };
+  var charSet = charSets[charType.toLowerCase()] || charSets.lowercase;
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
 }
+
+
+
